95c3d16e13d67795568bc502f63ccebfc1e39dbe
Senku <rabochaya.pochta670@gmail.com>
2024-09-17 01:33:39 +0300

    слайдер вновь не работает

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,27 @@
 "use strict";
 
-// const subnav = document.querySelector(".nav__link .has-subnav .subnav");
-// subnav.addEventListener()
+const slider = new Swiper(".slider__container", {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+  mousewheel: {
+
+  },
+
+  slidesPerView: 2,
+  spaceBetween: 40,
+
+});
+
