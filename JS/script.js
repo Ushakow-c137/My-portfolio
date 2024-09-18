56c818972ab8c49dfd0477ff3e2db826f84c2ba2
Senku <rabochaya.pochta670@gmail.com>
2024-09-19 02:58:23 +0300

    пошла вёрста, даже не ломается

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,16 @@
 "use strict";
 
+const pade = document.body
+const width = pade.clientWidth;
+let clientWidth = function () {
+  if (width >= 1500) {
+    return 76;
+  } else {
+    return 20
+  }
+};
+
+
 const slider = new Swiper(".slider", {
   loop: true,
 
@@ -22,7 +33,34 @@ const slider = new Swiper(".slider", {
   },
 
   slidesPerView: 2,
-  spaceBetween: 76,
+  spaceBetween: clientWidth(),
 
 });
 
+let styleMode = localStorage.getItem("styleMode");
+
+const styleToggle = document.querySelector('.header__switch');
+console.log(styleToggle);
+
+const enableDarkStyle = () => {
+  document.body.classList.add("light");
+  localStorage.setItem('styleMode', 'dark');
+};
+
+const disableDarkStyle = () => {
+  document.body.classList.remove("light");
+  localStorage.setItem('styleMode', null);
+};
+
+styleToggle.addEventListener('click', () => {
+  let styleMode = localStorage.getItem("styleMode");
+  if (styleMode !== 'dark') {
+    enableDarkStyle();
+  } else {
+    disableDarkStyle();
+  }
+});
+
+if (styleMode === 'dark') {
+  enableDarkStyle();
+}
